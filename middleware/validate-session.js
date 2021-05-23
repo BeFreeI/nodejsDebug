2a78a968b4794484ae95459599558341f3ed5e5d
BeFreeI <nepogodin_p@mail.ru>
2021-05-23 10:32:50 +0300

    fix: fix some problems

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -7,21 +7,22 @@ module.exports = (req, res, next) => {
     next();   // allowing options as a method for request
   } else {
     const sessionToken = req.headers.authorization;
+    console.log('=====' + sessionToken);
     if (!sessionToken) return res.status(403).send({ auth: false, message: "No token provided." });
     else {
       jwt.verify(sessionToken, SECRET_KEY, (err, decoded) => {
         if (decoded) {
           User.findOne({ where: { id: decoded.id } }).then(user => {
             req.user = user;
-            console.log(`user: ${user}`)
-            next()
+            console.log(`user: ${user}`);
+            next();
           },
             () => {
               res.status(401).send({ error: "not authorized" });
             })
 
         } else {
-          res.status(400).send({ error: "not authorized" })
+          res.status(400).send({ error: "not authorized" });
         }
       });
     }
